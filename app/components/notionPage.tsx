1eaf3d546d3368418d1c30a9573dfea47b7708d9
Noh <nhw3152@gmail.com>
2026-03-11 12:15:03 +0900

    fix: 소개 섹션 이미지 비율 수정 가능하게 .css 추가

--- app/components/notionPage.tsx
+++ app/components/notionPage.tsx
@@ -2,6 +2,7 @@
 
 import { NotionRenderer } from "react-notion-x";
 import "react-notion-x/src/styles.css";
+import "./notionPage.css";
 import type { ExtendedRecordMap } from "notion-types";
 
 interface Props {
@@ -10,10 +11,12 @@ interface Props {
 
 export default function NotionPage({ recordMap }: Props) {
     return (
-        <NotionRenderer
-            recordMap={recordMap}
-            fullPage={false}
-            darkMode={true}
-        />
+        <div className="notion-about">
+            <NotionRenderer
+                recordMap={recordMap}
+                fullPage={false}
+                darkMode={true}
+            />
+        </div>
     );
 }
